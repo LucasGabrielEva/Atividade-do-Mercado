2c36768a297de69a523b31bb1149d5a851be349f
Lucas Gabriel <lucasgabriel19672@gmail.com>
2025-12-03 21:35:01 -0300

    Delete Mercado directory

--- Mercado/src/main/java/com/example/Mercado/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Mercado.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Mercado.model.ProdutoModel;
-
-public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
-}
